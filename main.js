623e23ec1ca13070a3b64e6202e84e88cc3d83ea
heche <he.chen@estudiantat.upc.edu>
2022-09-26 21:33:25 +0200

    sss

--- main.js
+++ main.js
@@ -6631,14 +6631,16 @@ var mountCreep = () => {
 module.exports.loop = function () {
     MemHack.pretick();
     mountCreep();
-    new Colony('W1N7');
-    //colony.run();
+    const colony = new Colony('W7N9');
+    colony.run();
     const creep = Memory['creeps'];
     for (let creepName in creep) {
-        if (Game.creeps[creepName]) ;
+        if (Game.creeps[creepName]) {
+            Game.creeps[creepName]['work']();
+        }
     }
-    //Memory['colony']['W1N7']['creepSpawning']['spawn'].push('Spawn1')
-    //ColonyApi.createColony('W1N7')
+    //Memory['colony']['W7N9']['creepSpawning']['spawn'].push('Spawn1')
+    //ColonyApi.createColony('W7N9')
     //ColonyApi.destroyAllBuilding('W1N7')
     //ColonyApi.deleteColony('W1N7')
     //Memory.creeps = {}
